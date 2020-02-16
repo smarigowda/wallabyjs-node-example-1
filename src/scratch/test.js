2b982a1d61a2da12f0ba383b605fb10feb240697
Santosh Marigowda <santosharakere@gmail.com>
2020-02-16 17:20:32 +0000

    rename and cleanup

--- src/scratch/test.js
+++
@@ -1,9 +0,0 @@
-const instance = require("axios");
-const promise = instance.get(
-  "http://dummy.restapiexample.com/api/v1/employees"
-);
-
-(async function() {
-  const result = await promise;
-  console.log(result.data.data);
-})();
--- src/test/testSpec.js
+++ src/test/testSpec.js
@@ -1,13 +1,14 @@
 let calculator = require("../lib/calculator");
-let ajaxRequest = require('../lib/ajax');
+let ajaxRequest = require("../lib/ajax");
 
 describe("calculator", () => {
   it("should add numbers", () => {
     expect(calculator.add(2, 2)).to.equal(4);
   });
-  it("ajax request", async () => {
+  it("ajax request", async done => {
     let result = await ajaxRequest();
-    let { data } = result; //?
+    let { data } = result;
     expect(data.data.length).to.equal(24);
+    done();
   });
 });
