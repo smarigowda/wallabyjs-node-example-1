f907039eae285754043b725ecf403f96150e369c
Santosh Marigowda <santosharakere@gmail.com>
2020-02-16 12:19:44 +0000

    rest api example

--- src/test.js
+++ src/test.js
@@ -1,1 +1,9 @@
-console.log('--- test ---');
+const instance = require("axios");
+const promise = instance.get(
+  "http://dummy.restapiexample.com/api/v1/employees"
+);
+
+(async function() {
+  const result = await promise;
+  console.log(result.data.data);
+})();
